be2cf0313fa6d160d526d03abfb48d01218e8693
Hifi58 <gillet.tony@gmail.com>
2021-08-30 16:51:15 +0200

    mise en place bdd

--- biotope.php
+++ biotope.php
@@ -1,3 +1,8 @@
+<?php 
+require_once('database/connection.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +12,32 @@
     <link rel="stylesheet" href="style/style.css">
     <title>Monde en équilibre</title>
 </head>
-<body>
+<body class="biotope">
     
-    <?php require_once('assets/header.php'); ?>
+    <?php require_once('assets/header.php'); 
+    
+    $sql = "SELECT * FROM biotopes ORDER BY id_biotope DESC";
+    $query = $db->prepare($sql);
+    $query->execute();?>
 
+  <section>
+      <h2>Les différents biotopes</h2>
+   <?php while($data = $query->fetch()){?>
+
+
+    
+        
+        <div class="container">
+            <div class="wrapperbiotope">
+                <h3><?= $data['name'] ?></h3>
+                <p><?= $data['content'] ?></p>
+            </div>
+        </div>
+    
+
+<?php
+};
+?>
+</section>
 </body>
 </html>
