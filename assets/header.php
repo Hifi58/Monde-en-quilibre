a547e9ac8a7202810a0c7c4f846a377f0b02878d
Hifi58 <gillet.tony@gmail.com>
2021-08-30 14:50:16 +0200

    first commit

---
+++ assets/header.php
@@ -0,0 +1,6 @@
+<header>
+    <h1><span>M</span>onde <br/>en<br/> <span>É</span>quilibre</h1>
+    <nav>
+    
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Monde en équilibre</title>
+</head>
+<body>
+    
+    <?php require_once('assets/header.php'); ?>
+
+    <section>
+        <h2>Écosystème</h2>
+        <div class="container">
+            <div class="wrapper">
+                <h3>Biotope</h3>
+                <p>Aire géographique de dimensions variables, souvent de taille réduite, offrant des conditions constantes ou cycliques aux espèces constituant la biocénose. L’ensemble des facteurs physiques et chimiques de l’environnement reste sensiblement constant.</p>
+                <button><a href="biotope.php">En savoir plus</a></button>
+            </div>
+            <div class="wrapper">
+            <h3>Biocénose</h3>
+                <p>Ensemble des êtres vivants qui occupent un milieu donné (le biotope), en interaction les uns avec les autres et avec ce milieu. (La biocénose forme, avec son biotope, un écosystème.)</p>
+                <button><a href="">En savoir plus</a></button>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p>© Tony Gillet 2021</p>
+    </footer>
+    
+</body>
+</html>
